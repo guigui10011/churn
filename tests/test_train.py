f442580f238ecad108449a21cede6f272b26730c
guigui10011 <guigui.cavaro@aliceadsl.fr>
2025-02-11 16:13:22 +0000

    commit

--- tests/test_train.py
+++ tests/test_train.py
@@ -5,20 +5,20 @@ from sklearn.linear_model import LogisticRegression
 
 def test_train_model_file_exists():
     """Vérifie que le fichier churn_model_clean.pkl est créé après exécution de train.py"""
-    assert os.path.exists('models/model_sklearn.pkl'), (
+    assert os.path.exists('data/model_sklearn.pkl'), (
         "Le fichier model_sklearn.pkl n'existe pas après l'exécution de train.py."
     )
 
 def test_train_model_loading():
     """Vérifie que le fichier sauvegardé contient un modèle Random Forest"""
-    model = joblib.load('models/model_sklearn.pkl')
+    model = joblib.load('data/model_sklearn.pkl')
     assert isinstance(model, LogisticRegression), (
         "Le fichier model_sklearn.pkl ne contient pas un modèle LogisticRegression."
     )
 
 def test_train_model_prediction():
     """Vérifie que le modèle entraîné peut prédire sur un sous-ensemble des données"""
-    model = joblib.load('models/model_sklearn.pkl')
+    model = joblib.load('data/model_sklearn.pkl')
     data = pd.read_csv('data/customer_churn.csv')
     X = data[['Age', 'Account_Manager', 'Years', 'Num_Sites']]
 
